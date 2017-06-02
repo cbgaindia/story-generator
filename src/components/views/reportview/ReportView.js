444abe84c88874a98930a6885d1eaa8513bb1bb9
Akshay Verma <akshayverma295@gmail.com>
2017-06-03 00:41:40 +0530

    Add header structure mockup

--- src/components/views/reportview/ReportView.js
+++ src/components/views/reportview/ReportView.js
@@ -11,10 +11,48 @@ class ReportView extends React.Component {
 render(){
 
 	return(
-	<div className = "col-lg-10" >
-	    <div id = "vis-container" >
-	        <div id="card-container">
-	          Hello World{/*  
+		<div id="report-container">
+			<div id="header-container">
+				<div className="row">
+					<div className="col-lg-10">
+						<h3 className="primary-header">
+						Total Expenditure
+						</h3>
+					</div>
+					<div className="col-lg-2 see-details-text">
+						See Details
+					</div>
+					
+				</div>
+				<div className="row">
+					<div className="col-lg-8">
+						<h4 className="secondary-header">
+							Agriculture & Allied Activities
+						</h4>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-8">
+						<h5 className="addtional-details">
+							<span className="add-detail-1">
+							2016-17  
+							</span>
+							<span className="add-detail-2">
+							 | Budget Estimates
+							</span>
+						</h5>
+					</div>
+					<div className="col-lg-4">
+						<h5 className="unit-text addtional-details">Unit : Figures in INR (in Crores)</h5>
+					</div>
+				</div>
+
+
+			</div>
+
+	    	<div id = "vis-container" >
+	        
+	         {/*  
 	            <div className="row selected-params">
 					<div className="row">
 						<div className="col-lg-10 indicator-title-wrapper">
@@ -57,7 +95,7 @@ render(){
 	    */}
 	        </div>
 	    </div>
-	</div>)
+)
 }
 }
 
