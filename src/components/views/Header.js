5b3b37a14f9dad71a0bf8ff21c85f28fbba8b2a7
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-03 16:52:15 +0530

    Fix class warning

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -6,7 +6,7 @@ import { IndexLink } from 'react-router';
 export const Header = ({ params }) => {
     const activeTheme = (params != undefined && params.theme != undefined) ? themes.filter(t => t.url_slug === params.theme).shift() : themes[0];
     return (
-        <div class='header'>
+        <div className='header'>
             <div className="row-fluid">
 				<IndexLink to="/" className="story-generator-logo">
                 <h2 className="app-title"> Story Generator<sub className="alpha">ALPHA</sub> <hr className="title-hr" /></h2>
