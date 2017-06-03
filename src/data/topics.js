bb87dc819d0893d5ca624214ab466bd0ddf1f436
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-03 16:42:17 +0530

    Add topics

--- src/data/topics.js
+++ src/data/topics.js
@@ -2,21 +2,21 @@ export const topics = [
     {
         url_slug: 'legal-counsel-appointed',
         sub_theme_url_slug: 'access-to-legal-aid/appointments',
-        name: 'Number of districts where legal aid counsel were appointed for all magistrate courts/ ( Number of districts which responded to the RTI)',
+        name: 'Appointment of Legal Counsel',
         description: 'Number of districts where legal aid counsel were appointed for all magistrate courts/ ( Number of districts which responded to the RTI)',
         data: {}
     },
     {
         url_slug: 'panel-lawyers-appointed',
         sub_theme_url_slug: 'access-to-legal-aid/appointments',
-        name: 'Number of districts where panel lawyers were appointed/( Number of districts which responded to the RTI)',
+        name: 'Appointment of Panel Lawyers',
         description: 'Number of districts where panel lawyers were appointed/( Number of districts which responded to the RTI)',
         data: {}
     },
     {
         url_slug: 'retainer-lawyers-appointed',
         sub_theme_url_slug: 'access-to-legal-aid/appointments',
-        name: 'Number of districts where retainer lawyers were appointed/(Number of districts which responded to the RTI)',
+        name: 'Appointment of Retainer Lawyers',
         description: 'Number of districts where retainer lawyers were appointed/(Number of districts which responded to the RTI)',
         data: {}
     }
