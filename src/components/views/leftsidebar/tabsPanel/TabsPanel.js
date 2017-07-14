a8d00a786d702225c64706b38b2b560187a3180d
unknown <EP15@INLTD13106.global.tesco.org>
2017-07-14 14:52:16 +0530

    Added missing import statements after restructure

--- src/components/views/leftsidebar/tabsPanel/TabsPanel.js
+++ src/components/views/leftsidebar/tabsPanel/TabsPanel.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 import Tabs from './subComponents/Tabs';
+import PanelContent from './subComponents/PanelContent';
 
 class TabsPanel extends React.Component {
     constructor() {
--- src/components/views/leftsidebar/tabsPanel/subComponents/PanelList.js
+++ src/components/views/leftsidebar/tabsPanel/subComponents/PanelList.js
@@ -1,43 +1,46 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-export default class PanelList extends React.Component{
-    render(){
+
+import Records from './Records';
+
+export default class PanelList extends React.Component {
+    render() {
         let listItems = null;
-        let heirarchy_level = this.props.panelContent.heirarchy_level ;
-        if(heirarchy_level == 2)
-        {
+        let heirarchy_level = this.props.panelContent.heirarchy_level;
+        if (heirarchy_level == 2) {
             let panelTitle = this.props.panelContent.title_slug;
-            listItems = this.props.panelContent.data.map(function(category,index){
+            listItems = this.props.panelContent.data.map(function (category, index) {
                 return (
                     <div className="panel panel-default" key={index}>
                         <div className="panel-heading">
                             <h4 className="panel-title">
-                                <a data-toggle="collapse" className="collapsed" data-parent="#accordion" href={"#" + category.category_slug} >{category.category_name}</a>
+                                <a data-toggle="collapse" className="collapsed" data-parent="#accordion"
+                                   href={"#" + category.category_slug}>{category.category_name}</a>
                             </h4>
                         </div>
                         <div id={category.category_slug} className="panel-collapse collapse ">
-                            <Records 	categoryName={category.category_name}
-                                        subRecords={category.sub_records}
-                                        slugCategory={category.category_slug}
-                                        panelTitle={panelTitle}
-                                        heirarchyLevel={heirarchy_level} />
+                            <Records categoryName={category.category_name}
+                                     subRecords={category.sub_records}
+                                     slugCategory={category.category_slug}
+                                     panelTitle={panelTitle}
+                                     heirarchyLevel={heirarchy_level}/>
                         </div>
                     </div>
                 );
             });
         }
 
-        else if(heirarchy_level == 1){
+        else if (heirarchy_level == 1) {
             listItems = (
-                <Records 	categoryName={"null"}
-                            subRecords={this.props.panelContent.data}
-                            panelTitle={this.props.panelContent.title_slug}
-                            heirarchyLevel={heirarchy_level} />
+                <Records categoryName={"null"}
+                         subRecords={this.props.panelContent.data}
+                         panelTitle={this.props.panelContent.title_slug}
+                         heirarchyLevel={heirarchy_level}/>
             );
         }
 
         return (
-            <div className="panel-group select-panel" id="accordion" >
+            <div className="panel-group select-panel" id="accordion">
                 {listItems}
             </div>
         );
@@ -45,5 +48,5 @@ export default class PanelList extends React.Component{
 }
 
 PanelList.propTypes = {
-    panelContent:React.PropTypes.object
+    panelContent: React.PropTypes.object
 };
