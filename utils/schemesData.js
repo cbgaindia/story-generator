88c4b8ee2758c1040aa3e9bc1eff53fd25d5df68
Abhinav Kumar <a.kumar2102@gmail.com>
2022-01-28 20:14:08 +0530

    ADDED - receipts indicator and  other new sector

--- utils/schemesData.js
+++ utils/schemesData.js
@@ -1,7 +1,8 @@
 import health from 'public/Images/schemesLogos/health.png';
 
 import agri 	from 'public/Images/schemesLogos/Agriculture and Allied Activities.png';
-import drinking from 'public/Images/schemesLogos/Drinking Water and Sanitation.png';
+import agg_bud  from 'public/Images/schemesLogos/Aggregate Budget Indicators.png';
+import rcpt_ind from 'public/Images/schemesLogos/Receipts Indicators.png';
 import edu 	from 'public/Images/schemesLogos/Education.png';
 import food	from 'public/Images/schemesLogos/Food, Civil Supplies and Cooperation.png';
 import forest	from 'public/Images/schemesLogos/Forests and Environment.png';
@@ -13,15 +14,17 @@ import social	from 'public/Images/schemesLogos/Social Welfare.png';
 import urban    from 'public/Images/schemesLogos/Urban Development and Housing.png';
 
 
-
-
-
 const SchemesData = {
   dash_desc: 'The Sector Dashboard is a tool that facilitates comparison of aggregate budget indicators across different states and years. The indicators of expenditure data have been classified into eleven social and economic sectors. It enables users to make inter-state comparison of budget data by generating simple and interactive visualisations, which can be downloaded for reuse. Detailed descriptions are included to help users understand the specificities of calculation for the indicators presented. The dashboard also provides data on a few macro and receipts indicators across states.',
   health: {
     logo: health,
     data_count: 1,
     desc: 'Health',
+  },
+  receipts: {
+    logo: rcpt_ind,
+    data_count: 1,
+    desc: 'receipts',
   },
   comingsoon : [
           {
@@ -32,11 +35,11 @@ const SchemesData = {
 	    desc: 'Agriculture and Allied Activities',
 	  },
 	  {
-	    title: "Drinking Water and Sanitation",
+	    title: "Aggregate Budget Indicators",
 	    slug: "#",
-	    logo: drinking,
+	    logo: agg_bud,
 	    data_count: 0,
-	    desc: 'Drinking Water and Sanitation',
+	    desc: 'Aggregate Budget Indicators',
 	  },
 	  {
 	    title: "Education",
